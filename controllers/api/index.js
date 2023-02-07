9864af813da9b1164b2d292fc65f75c5af1b3b18
guscarrillo <115580058+guscarrillo@users.noreply.github.com>
2023-02-06 20:50:10 -0700

    cleaned up project files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const projectRoutes = require('./recipieRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/recipies', projectRoutes);
 
 module.exports = router;
