544f6c4e2be557366e93238d4851670734baea78
Kameron <kfarshchian@gmail.com>
2023-02-07 15:41:53 -0700

    api call working

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,47 @@
-let keyword = document.getElementById("keyword");
 const apiId = 'a4effe10';
 const apiKey = 'f944412b3011c0f498d615d4abf9bcf6';
-let diet = document.getElementById("diet");
-let health = document.getElementById("health");
-let cuisineType = document.getElementById("cuisineType");
-let mealType = document.getElementById("mealType");
-let calories = document.getElementById("calories");
-const imageSize = document.getElementById("imageSize");
+let submitButton = document.getElementById("search");
+const imageSize = 1;
 
-function getApi() {
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    let diet = document.getElementById("diet").value;
+    let health = document.getElementById("health").value;
+    let cuisineType = document.getElementById("cuisineType").value;
+    let mealType = document.getElementById("mealType").value;
+    let calories = document.getElementById("calories").value;
+    let keyword = document.getElementById("keyword").value;
 
-const url = 'https://api.edamam.com/api/recipes/v2?type=public&q=${Keyword}&app_id=${apiID}&app_key=${apiKey}&diet=${diet}&health=${health}&cuisineType=${cuisineType}&mealType=${mealType}&calories=${calories}&imageSize=&{imageSize}';
+    let apiURL = [`https://api.edamam.com/api/recipes/v2?type=public&q=${keyword}&app_id=${apiId}&app_key=${apiKey}`];
+   
 
-    fetch(requestUrl)
+    if (diet !== ""){
+        apiURL.concat(`&diet=${diet}`)
+    }
+    if(health !== ""){
+        apiURL.push(`&health=${health}`)
+    }
+    if(cuisineType !== ""){
+        apiURL.push(`&cuisineType=${cuisineType}`)
+    }
+    if(mealType !== ""){
+        apiURL.push(`&mealType=${mealType}`)
+    }
+    if(calories !== ""){
+        apiURL.push(`&calories=${calories}`)
+    }
+    if(imageSize !== ""){
+        apiURL.push(`&imageSize=THUMBNAIL`)
+    }
+
+    console.log(apiURL.join(''));
+    
+    fetch(apiURL.join(''))
     .then(function (response){
         return response.json();
     })
     .then(function (data) {
         console.log(data);
     })
+  });
 
-}
-
-getApi();
-console.log(getApi);
